564dc042d03abcade0bff994b368c2d3252b4c52
Saweron <sam@finesketch.ca>
2020-07-14 07:59:22 -0600

    added basic collision detection

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 --todo
---add collision
+--add collision detection //
+--add actual collision
 ---add gravity
 --add platforms
 --add camera movement
@@ -17,6 +18,21 @@ function decelerate(vel,time)
 end
 
 
+function isColliding(x,y,w,h,cx,cy,cw,ch)
+    -- if x >= cx and y >= cy and x+w <= cx+cw and y + h <= cy+ch then
+    --     return true
+    -- end
+    
+    -- if x + w > cx+cw or y + h > cy + ch then return false end
+    -- if x + w < cx or y < cy then return false end
+
+    if x > cx and x < cx+cw 
+    and y > cy and y < cy+ch
+    or x+w > cx and x+w < cx+cw
+    and y+h > cy and y+h < cy+ch then return true end
+    return false
+end
+
 function love.load()
     playerX = 0 
     playerY = 0
@@ -30,7 +46,7 @@ end
 function love.update(time)
     local moving = false
     local speed = 800
-    if love.keyboard.isDown("up") then      
+    if love.keyboard.isDown("up") then
         playerYV = playerYV + time*speed
         moving = true
     end
@@ -39,7 +55,7 @@ function love.update(time)
         moving = true
     end
     if love.keyboard.isDown("right") then
-        playerXV = playerXV + time*speed            
+        playerXV = playerXV + time*speed
         moving = true
     end
     if love.keyboard.isDown("left") then
@@ -57,6 +73,7 @@ function love.update(time)
         playerYV = decelerate(playerYV,time)
         playerXV = decelerate(playerXV,time) 
     end
+    print(isColliding(playerX,playerY,10,10,200,200,200,200))
 end
 
 function love.draw()
@@ -67,4 +84,6 @@ function love.draw()
     love.graphics.scale(1,-1)
     love.graphics.print("Hello World", 400, 300)
     love.graphics.rectangle("fill",playerX,playerY,10,10)
+
+    love.graphics.rectangle("fill",200,200,200,200)
 end
